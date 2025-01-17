7114a00675360fbba8b32ef4461657245632840e
RanchMonster <Ranchmonster06@outlook.com>
2025-01-17 11:16:43 -1000

    added course button

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,10 +1,10 @@
 import { View, Text, StyleSheet, Pressable } from "react-native"
 import loadAssets from "../logics/Assetsloader";
-type ButtonProps = {
+export type ButtonProps = {
     onClick: () => void
-    content: String
 }
-type HeaderButtonProps = ButtonProps & {
+export type HeaderButtonProps = ButtonProps & {
+    content: String
 
 }
 export function HeaderButton(props: HeaderButtonProps): JSX.Element {
@@ -12,14 +12,33 @@ export function HeaderButton(props: HeaderButtonProps): JSX.Element {
     return (
         <View style={{ paddingHorizontal: "2%" }}>
             <Pressable onPress={props.onClick}>
-                <View style={[headerButtonStyles.buttonStyle, {shadowColor:assets.background, backgroundColor: assets.surface }]}>
+                <View style={[styles.buttonStyle, { shadowColor: assets.background, backgroundColor: assets.surface }]}>
                     <Text style={{ color: assets.text, fontSize: 30, fontFamily: assets.boldFont as string }}>{props.content}</Text>
                 </View>
             </Pressable>
         </View>
 
     );
-} const headerButtonStyles = StyleSheet.create({
+}
+export type CourseButtonProps = ButtonProps & {
+    name: String
+    description: String
+}
+export function CourseButton(props: CourseButtonProps): JSX.Element {
+    const assets = loadAssets()
+    return (
+        <Pressable style={[styles.courseCard, { backgroundColor: assets.surface }]} onPress={props.onClick}>
+            <Text style={[styles.courseName, { color: assets.text, fontFamily: assets.boldFont.toString() }]}>
+                Course: {props.name}
+            </Text>
+            <Text style={[styles.courseDescription, { color: assets.text, fontFamily: assets.normFont.toString() }]}>
+                Description: {props.description}
+            </Text>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
     buttonStyle: {
         flex: 1,
         padding: 10,
@@ -33,5 +52,21 @@ export function HeaderButton(props: HeaderButtonProps): JSX.Element {
         },
         shadowOpacity: 0.25,
         shadowRadius: 3.84,
+    },
+    courseCard: {
+        marginBottom: 10,
+        padding: 15,
+        borderRadius: 10,
+        borderWidth: 1,
+    },
+    courseName: {
+        fontSize: 18,
+        // fontWeight: "bold",
+        marginBottom: 5,
+    },
+    courseDescription: {
+        fontSize: 14,
+        lineHeight: 20,
+        textAlign: "justify",
     },
 })
