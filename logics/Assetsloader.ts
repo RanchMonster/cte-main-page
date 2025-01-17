4fc8ef64e1837b1fdad92d6269c31a69c888bea8
RanchMonster <Ranchmonster06@outlook.com>
2025-01-17 10:09:42 -1000

    created the basic assetsloader to load all need assets

--- logics/Assetsloader.ts
+++ logics/Assetsloader.ts
@@ -1,6 +1,6 @@
-import { light, dark } from "../assets/colors.json"
+import { colors } from "../assets/colors.json"
 import { ColorValue, useColorScheme } from "react-native"
-import { useFonts, AdventPro_400Regular, AdventPro_700Bold,AdventPro_800ExtraBold } from "@expo-google-fonts/advent-pro"
+import { useFonts, AdventPro_400Regular, AdventPro_700Bold, AdventPro_800ExtraBold } from "@expo-google-fonts/advent-pro"
 type BaseAssets = {
     text: ColorValue
     surface: ColorValue
@@ -11,7 +11,6 @@ type BaseAssets = {
 
 }
 let assets: BaseAssets = undefined
-let loaded: boolean = false
 export default function loadAssets(): BaseAssets {
 
     const colorScheme = useColorScheme()
@@ -20,26 +19,13 @@ export default function loadAssets(): BaseAssets {
         AdventPro_800ExtraBold
     })
     if (fontloaded) {
-
-        if (colorScheme === 'light') {
-            assets = {
-                text: light.text,
-                surface: light.surface,
-                primary: light.primary,
-                background: light.background,
-                normFont: "AdventPro_400Regular",
-                boldFont: "AdventPro_800ExtraBold"
-
-            }
-        } else {
-            assets = {
-                text: dark.text,
-                surface: dark.surface,
-                primary: dark.primary,
-                background: dark.background,
-                normFont: "AdventPro_400Regular",
-                boldFont: "AdventPro_800ExtraBold"
-            }
+        assets = {
+            text: colors.text,
+            surface: colors.surface,
+            primary: colors.primary,
+            background: colors.background,
+            normFont: "AdventPro_400Regular",
+            boldFont: "AdventPro_800ExtraBold"
         }
     }
     return assets
