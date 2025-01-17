325ad41e327989bb4e9cc7b6da3816fb31cc6434
RanchMonster <Ranchmonster06@outlook.com>
2025-01-17 11:14:01 -1000

    switched to a new font and started loading all images at the same time in loadasstes instead of where ever they are being used

--- logics/Assetsloader.ts
+++ logics/Assetsloader.ts
@@ -1,6 +1,6 @@
 import { colors } from "../assets/colors.json"
 import { ColorValue, useColorScheme } from "react-native"
-import { useFonts, AdventPro_400Regular, AdventPro_700Bold, AdventPro_800ExtraBold } from "@expo-google-fonts/advent-pro"
+import { useFonts, Poppins_400Regular, Poppins_700Bold } from "@expo-google-fonts/poppins"
 type BaseAssets = {
     text: ColorValue
     surface: ColorValue
@@ -8,24 +8,27 @@ type BaseAssets = {
     background: ColorValue
     normFont: String
     boldFont: String
+    logo: any
 
 }
 let assets: BaseAssets = undefined
 export default function loadAssets(): BaseAssets {
-
     const colorScheme = useColorScheme()
     const [fontloaded] = useFonts({
-        AdventPro_400Regular,
-        AdventPro_800ExtraBold
+        Poppins_400Regular,
+        Poppins_700Bold
     })
     if (fontloaded) {
-        assets = {
-            text: colors.text,
-            surface: colors.surface,
-            primary: colors.primary,
-            background: colors.background,
-            normFont: "AdventPro_400Regular",
-            boldFont: "AdventPro_800ExtraBold"
+        if (assets === undefined) {
+            assets = {
+                text: colors.text,
+                surface: colors.surface,
+                primary: colors.primary,
+                background: colors.background,
+                normFont: "Poppins_400Regular",
+                boldFont: "Poppins_700Bold",
+                logo: require("../assets/logo.png")
+            }
         }
     }
     return assets
